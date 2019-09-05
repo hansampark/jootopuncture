3d0612d690ad384da8948c29fdbde6838004723a
Hansam Park <hansampark@Hansams-MacBook-Pro.local>
2019-09-04 19:00:00 -0700

    implement redux-persist for state persistant

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,17 +1,28 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import monitorReducersEnhancer from '../lib/monitorReducer';
 import loggerMiddleware from '../middleware/logger';
 import rootReducer from '../reducers';
 
+const persistConfig = {
+  key: 'root',
+  storage
+};
 export default function configureStore(preloadedState) {
   const middlewares = [loggerMiddleware, thunkMiddleware];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
   const enhancers = [middlewareEnhancer, monitorReducersEnhancer];
   const composedEnhancers = compose(...enhancers);
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  const store = createStore(
+    persistedReducer,
+    preloadedState,
+    composedEnhancers
+  );
+  const persistor = persistStore(store);
 
-  const store = createStore(rootReducer, preloadedState, composedEnhancers);
-  return store;
+  return { store, persistor };
 }
